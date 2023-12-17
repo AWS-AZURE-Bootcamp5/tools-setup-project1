f5ca63f004cb085840ce0c687b08842239d05bae
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-17 15:29:11 +0530

    Create kubectl.sh

---
+++ kubectl.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install curl -y
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
